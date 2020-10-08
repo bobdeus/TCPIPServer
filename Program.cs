d2b8d7b806a1145f429902d0afae424f5186d0c7
ddixon <ddixon@mastercontrol.com>
2020-10-07 19:50:48 -0600

    Add Server

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPIPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+            TcpListener listener = new TcpListener(iPAddress, 5001);
+            listener.Start();
+            Console.WriteLine($"Server started on: {listener.LocalEndpoint}");
+            Socket socket = listener.AcceptSocket();
+            Console.WriteLine($"Someone connected on: {socket.RemoteEndPoint}");
+
+            byte[] bytes = new byte[4096];
+            int remainingBytes = socket.Receive(bytes);
+            for (int i = 0; i < remainingBytes; i++)
+            {
+                Console.Write(Convert.ToChar(bytes[i]));                
+            }
+
+            socket.Send(Encoding.ASCII.GetBytes("The server got stuff"));
+            socket.Close();
+            listener.Stop();
+        }
+    }
+}
